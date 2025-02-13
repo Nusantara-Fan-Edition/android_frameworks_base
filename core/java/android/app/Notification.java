af5be66b5d330cb5220d13ca6f93f259f84b653a
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-02-13 13:34:27 +0800

    Reduce notification memory usage
    Change-Id: I75a99e79f357874cf3e229b442b5142c311f49a0
    Signed-off-by: OliverSyx <173170056+OliverSyx@users.noreply.github.com>
    Signed-off-by: Rimuru <paleloebarbershop@gmail.com>

--- core/java/android/app/Notification.java
+++ core/java/android/app/Notification.java
@@ -6208,7 +6208,7 @@ public class Notification implements Parcelable
         if (extras.getBoolean(EXTRA_REDUCED_IMAGES)) {
             return;
         }
-        boolean isLowRam = ActivityManager.isLowRamDeviceStatic();
+        boolean isLowRam = true;
         if (mLargeIcon != null || largeIcon != null) {
             Resources resources = context.getResources();
             Class<? extends Style> style = getNotificationStyle();
@@ -6720,7 +6720,7 @@ public class Notification implements Parcelable
         public void reduceImageSizes(Context context) {
             super.reduceImageSizes(context);
             Resources resources = context.getResources();
-            boolean isLowRam = ActivityManager.isLowRamDeviceStatic();
+            boolean isLowRam = true;
             if (mPicture != null) {
                 int maxPictureWidth = resources.getDimensionPixelSize(isLowRam
                         ? R.dimen.notification_big_picture_max_height_low_ram
