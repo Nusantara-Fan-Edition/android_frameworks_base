876ed7bb77c4e0b7f38057d7fe9244ef44f8ba9a
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-02-13 13:25:13 +0800

    Disable notification cache
    Change-Id: Ia841dfc8f87fe9fc9c76785e4165890a47071424
    Signed-off-by: OliverSyx <173170056+OliverSyx@users.noreply.github.com>
    Signed-off-by: Rimuru <paleloebarbershop@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/row/NotificationInlineImageResolver.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/row/NotificationInlineImageResolver.java
@@ -67,7 +67,7 @@ public class NotificationInlineImageResolver implements ImageResolver {
      * @return True if has its internal cache, false otherwise.
      */
     public boolean hasCache() {
-        return mImageCache != null && !ActivityManager.isLowRamDeviceStatic();
+        return false;
     }
 
     /**
